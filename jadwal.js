d592740651e3e8a382ccd397b4f3ab54266d8094
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-08-20 00:07:51 +0700

    push new

--- jadwal.js
+++ jadwal.js
@@ -1,2 +1,20 @@
+// const axios = require("axios");
 
+// async function doGetRequest() {
+//   let res = await axios.get(
+//     "https://api.myquran.com/v1/sholat/jadwal/1609/2022/08/19"
+//   );
+
+//   let data = res.data.data.jadwal;
+//   let tanggal = data["tanggal"];
+//   let subuh = data["subuh"];
+//   let dzuhur = data["dzuhur"];
+//   let ashar = data["ashar"];
+//   let magrib = data["maghrib"];
+//   let isya = data["isya"];
+
+//   console.log(data);
+// }
+
+// doGetRequest();
 
